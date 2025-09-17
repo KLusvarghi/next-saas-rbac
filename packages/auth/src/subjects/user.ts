f521441cb5230551e524b5eb7f8c75743b47dec1
KLusvarghi <kauaolusvarghi@gmail.com>
2025-09-17 18:43:30 -0300

    feat: create userSubject schema using Zod for user actions and type inference

---
+++ packages/auth/src/subjects/user.ts
@@ -0,0 +1,18 @@
+// é um array de 2 posições, a primeira posição é uma action e a segunda posição é um subject
+
+import z from "zod";
+
+// e usando "array" nós podemos adicionar mais posições, o que não é o caso de uma tupla
+export const userSubject = z.tuple([
+  // como a primeira posição são actions, e nelas utilizamos "ou" (|), nós podemos usar o "z.union" para unir as actions
+  // e não podemos passar os valores diretamente, temos que tipar esses valores com o "z.literal", usamos o "z.literal" porque tem que ser exatamente a string que passamos
+  z.union([
+    z.literal("manager"),
+    z.literal("get"),
+    z.literal("update"),
+    z.literal("delete"),
+  ]),
+  z.literal("User"),
+]);
+
+export type UserSubject = z.infer<typeof userSubject>;
