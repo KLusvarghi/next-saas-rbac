3a0d467fb474e1a8e44b758389adc4999e6debc5
KLusvarghi <kauaolusvarghi@gmail.com>
2025-09-17 20:22:12 -0300

    feat: add id field to userSchema for enhanced user identification

--- packages/auth/src/models/user.ts
+++ packages/auth/src/models/user.ts
@@ -3,12 +3,8 @@ import z from "zod";
 import { roleSchema } from "../roles";
 
 export const userSchema = z.object({
+  id: z.string(),
   role: roleSchema,
 });
-// name: string
-// email: string
-// password: string
-// createdAt: Date
-// updatedAt: Date
 
 export type User = z.infer<typeof userSchema>;
