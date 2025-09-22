1f4ad2bc2cb087fe851060040a619fd8366b0fa0
KLusvarghi <kauaolusvarghi@gmail.com>
2025-09-22 12:58:59 -0300

    chore: update eslint configuration to disable conflicting rules with Prettier

--- config/eslint-config/node.js
+++ config/eslint-config/node.js
@@ -2,10 +2,19 @@
  * @type {import("eslint").Linter.Config[]}
  */
 module.exports = {
-  extends: ["@rocketseat/eslint-config/node"],
-  plugins: ["simple-import-sort"],
+  extends: ['@rocketseat/eslint-config/node'],
+  plugins: ['simple-import-sort'],
   rules: {
     // deixando como "error" ao rodar o lint ele irá identificar e corrigir os imports automaticamnete, diferente do "warn" que apenas avisa
-    "simple-import-sort/imports": "error",
+    'simple-import-sort/imports': 'error',
+
+    // Desabilita regras do ESLint que conflitam com o Prettier
+    quotes: 'off',
+    semi: 'off',
+    '@typescript-eslint/quotes': 'off',
+    '@typescript-eslint/semi': 'off',
+
+    // Desabilita o plugin prettier do ESLint para evitar conflitos
+    'prettier/prettier': 'off',
   },
-};
+}
