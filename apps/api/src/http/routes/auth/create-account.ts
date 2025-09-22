51b13278e7d2dc134e4b686f7b867f1a313851a8
KLusvarghi <kauaolusvarghi@gmail.com>
2025-09-22 16:18:49 -0300

    feat: enhance create account endpoint with Swagger documentation and auto-join organization feature

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,7 +1,8 @@
+// import fastifySwaggerUI from "@fastify/swagger-ui";
 import { hash } from "bcryptjs";
 import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from "zod";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod/v4";
 
 import { prisma } from "@/lib/prisma";
 
@@ -12,6 +13,17 @@ export async function createAccount(app: FastifyInstance) {
     // dentro desse objeto colocamos algumas confgs, e uma delas é o schema
     {
       schema: {
+        summary: "Create a new user",
+        description: "Create a new user",
+        tags: ["auth"],
+        response: {
+          201: z.object({
+            message: z.string(),
+          }),
+          400: z.object({
+            message: z.string(),
+          }),
+        },
         body: z.object({
           name: z.string(),
           email: z.email(),
@@ -34,6 +46,17 @@ export async function createAccount(app: FastifyInstance) {
         });
       }
 
+      // pegando apenas o dominio do email
+      const [, domain] = email.split("@");
+
+      // procuramos se existe alguma organization com tem aquele dominio pré configurado
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUserByDomain: true,
+        },
+      });
+
       // o 6 é a forca do hash, quanto maior o numero, mais forte o hash
       // e quando maior, mais implica em performance
       const passwordHash = await hash(password, 6);
@@ -43,6 +66,15 @@ export async function createAccount(app: FastifyInstance) {
           name,
           email,
           passwordHash,
+          // e aqui inserimos ou não o usuário a uma organização
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                  // nao inserimos a role, pq por padrão já é MEMBER
+                },
+              }
+            : undefined,
         },
       });
 
