46ee57ce296e3ecb0ebf6b7d7db70d1f3af79193
KLusvarghi <kauaolusvarghi@gmail.com>
2025-09-22 18:49:33 -0300

    refactor: integrate authentication middleware in get profile route

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -2,58 +2,64 @@ import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
 
+import { auth } from "@/http/middleware/auth";
 import { prisma } from "@/lib/prisma";
 
 import { NotFoundError } from "../_errors";
 
 export async function getProfile(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    "/profile",
-    {
-      schema: {
-        summary: "Get authenticated user profile",
-        description: "Get authenticated user profile",
-        tags: ["auth"],
-        headers: z.object({
-          authorization: z.string(),
-        }),
-        response: {
-          200: z.object({
-            message: z.string(),
-            user: z.object({
-              id: z.string(),
-              // o nullable é para que o zod possa aceitar valores nulos
-              name: z.string().nullable(),
-              email: z.string(),
-              avatarUrl: z.url().nullable(),
-            }),
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth) // registrando o middleware de autenticação
+    .get(
+      "/profile",
+      {
+        schema: {
+          summary: "Get authenticated user profile",
+          description: "Get authenticated user profile",
+          tags: ["auth"],
+          headers: z.object({
+            authorization: z.string(),
           }),
+          response: {
+            200: z.object({
+              message: z.string(),
+              user: z.object({
+                id: z.string(),
+                // o nullable é para que o zod possa aceitar valores nulos
+                name: z.string().nullable(),
+                email: z.string(),
+                avatarUrl: z.url().nullable(),
+              }),
+            }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const { sub } = await request.jwtVerify<{ sub: string }>();
+      async (request, reply) => {
+        // const { sub } = await request.jwtVerify<{ sub: string }>();
+        const userId = await request.getCurrentUserId();
 
-      const user = await prisma.user.findUnique({
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          avatarUrl: true,
-        },
-        where: {
-          id: sub,
-        },
-      });
+        const user = await prisma.user.findUnique({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatarUrl: true,
+          },
+          where: {
+            // id: sub,
+            id: userId,
+          },
+        });
 
-      if (!user) {
-        throw new NotFoundError("User not found");
-      }
+        if (!user) {
+          throw new NotFoundError("User not found");
+        }
 
-      return reply.status(200).send({
-        message: "Profile fetched successfully",
-        user,
-      });
-    },
-  );
+        return reply.status(200).send({
+          message: "Profile fetched successfully",
+          user,
+        });
+      },
+    );
 }
